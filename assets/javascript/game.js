f8d8062cb81fb5f6e6147f93142843ea5ae8f4c2
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-02-22 19:53:57 -0600

    updated JS file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,4 +2,10 @@ $(document).ready(function () {
     var healthPoints;
     var attackPower;
     var counterAttackPower;
+
+    healthPoints = Math.floor((Math.random() * 150) + 200);
+
+    $('.characters').on("click", function () {
+        $('.healthPoints').html(healthPoints);
+    });
 });
