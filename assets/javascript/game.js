2b50cdba27402692f1878e1552e6ef67bf924394
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-02-23 07:16:32 -0600

    added styling for red background, updated jQuery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,27 @@
 $(document).ready(function () {
-    var healthPoints;
-    var attackPower;
-    var counterAttackPower;
+    // Randomly generate health points for each character
+    var character1 = Math.floor((Math.random() * 300));
+    var character2 = Math.floor((Math.random() * 300));
+    var character3 = Math.floor((Math.random() * 300));
+    var character4 = Math.floor((Math.random() * 300));
 
-    healthPoints = Math.floor((Math.random() * 150) + 200);
+    // Assign health points to each character
+    $("#character1").html(character1);
+    $("#character2").html(character2);
+    $("#character3").html(character3);
+    $("#character4").html(character4);
 
-    $('.characters').on("click", function () {
-        $('.healthPoints').html(healthPoints);
+    $(".characters").on("click", function () {
+        $(this).siblings().removeClass("characters");
+        $(this).siblings().addClass("enemy");
+
+        $(".enemies").append($(this).siblings());
+        $("#player1").prepend($(this));
+        // $(this).siblings().detach();
+    });
+
+    $(".enemies").on("click", function () {
+        $("#player2").append($(this));
+        // $(".enemies").append($(this).siblings());
     });
 });
